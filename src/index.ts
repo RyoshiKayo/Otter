0c0db6d677ae114a4150e85a20edf19614b80f50
Kayo <kayo@kayo.moe>
2021-02-28 13:27:05 -0800

    fix(Build): Remove env variable checks

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,7 @@ import { config } from 'dotenv';
 config();
 import * as Commando from 'discord.js-commando';
 
-if (!process.env.DISCORD_BOT_TOKEN) {
-  throw new Error('DISCORD_BOT_TOKEN must be defined!');
-}
-
-if (!process.env.DISCORD_BOT_OWNER) {
-  throw new Error('DISCORD_BOT_OWNER must be defined!');
-}
-
-let PREFIX: string;
-if (process.env.DISCORD_BOT_PREFIX) {
-  PREFIX = process.env.DISCORD_BOT_PREFIX;
-  console.log(`Using prefix: ${PREFIX}`);
-} else {
-  throw new Error('DISCORD_BOT_TOKEN must be defined!');
-}
+let PREFIX: string = process.env.DISCORD_BOT_PREFIX;
 
 const client = new Commando.Client({
   owner: process.env.DISCORD_BOT_OWNER,
