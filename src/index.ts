fce5a08954f3b252ea8825aa8330be73d0e68324
Kayo <kayo@kayo.moe>
2021-03-10 19:23:24 -0800

    feat(AWS): Open port 80 for LoadBalancer
    This allows the AWS LoadBalancer to perform a health check, allowing it to be marked as healthy.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 config();
 import { ShardingManager } from 'discord.js';
+import * as http from 'http';
 
 const manager = new ShardingManager('./lib/bot.js', {
   token: process.env.DISCORD_BOT_TOKEN,
@@ -12,3 +13,11 @@ const manager = new ShardingManager('./lib/bot.js', {
 manager.on('shardCreate', (shard) => console.log(`Launched shard ${shard.id}`));
 
 manager.spawn();
+
+// To make the LoadBalancer register us as a healthy host.
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Loadbalancer port check\n');
+  })
+  .listen(80);
